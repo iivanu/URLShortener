f53fd796ec0eb3e186934b944565ca365503e4cd
Sergio Fresneda <sergio.fresneda@colba.es>
2020-10-27 22:00:13 +0100

    Added some best practices to the code
    * Added comments on each change

--- URL Shortener/URL Shortener/DataModel.swift
+++ URL Shortener/URL Shortener/DataModel.swift
@@ -7,17 +7,23 @@
 
 import Foundation
 
-let token = "c798d97ad43267d09a2eab588a954fc52c0f84a4"
-let apiURL = URL(string: "https://api-ssl.bitly.com/v4/shorten")!
+/// Add the "k" keyword before the name is an best practice from Objective-C
+/// this helps to recognize the difference between a local variable and a global constant
+/// works with the same efficience, but looks more readable. You can user any prefix, but always use the same.
+let k_token = "c798d97ad43267d09a2eab588a954fc52c0f84a4"
+let k_apiURL = URL(string: "https://api-ssl.bitly.com/v4/shorten")!
 
-class ResponseDataOK: NSObject, Codable {
+/// You don't need neither inheritance from NSObject on these models, you need only follow the Codable protocols.
+/// You don't need reference vars, you gonna set a value on your model and it will be done
+/// Following this sentences, you need an **Struct** instead a **Class**, is more lightfull, and works as expected.
+struct ResponseDataOK: Codable {
     var created_at: String
     var id: String
     var link: String
     var long_url: String
 }
 
-class ResponseDataNotOK: Codable {
+struct ResponseDataNotOK: Codable {
     var message: String
     var resource: String
     var description: String
--- URL Shortener/URL Shortener/ViewController.swift
+++ URL Shortener/URL Shortener/ViewController.swift
@@ -6,59 +6,81 @@
 //
 
 import UIKit
+/// Allow to summon a Safari View Controller, check the code below
+import SafariServices
 
 class ViewController: UIViewController {
-    @IBOutlet var urlEntry: UITextField!
-    @IBOutlet var shortLinkView: UITextField!
-    @IBOutlet var submitButton: UIButton!
-    @IBOutlet var copyButton: UIButton!
-    @IBOutlet var openPageButton: UIButton!
+    /// Always your UI elements should be **weak**, why? because your UI elements gonna take a **strong** reference from VC
+    /// and your VC never gonna deallocate, although the view never appears again. This is called *memory leaks*.
+    @IBOutlet weak var urlEntry: UITextField!
+    @IBOutlet weak var shortLinkView: UITextField!
+    @IBOutlet weak var submitButton: UIButton!
+    @IBOutlet weak var copyButton: UIButton!
+    @IBOutlet weak var openPageButton: UIButton!
     
-    var currentResponse: ResponseDataOK?
-    var notOKResponse: ResponseDataNotOK?
-    var recentLinks = [ResponseDataOK]()
+    /// All your variables should be private, to avoid other classes acces to it without your authorization,
+    /// to authorize the access may could be interesting do a getter setter, or if it's necesary to access to
+    /// some var, ok, make it public (not private), but try to privatize always your vars.
+    private var currentResponse: ResponseDataOK?
+    private var notOKResponse: ResponseDataNotOK?
+    /// If you need to set a default value on a var, try always to put it as **Lazy**, this allow to the app
+    /// ignore his value (do not initialize) until needed, what means this? More memory free, only used when you need
+    private lazy var recentLinks: [ResponseDataOK] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        loadData()
-        title = "URL shortener"
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
-        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Recent", style: .plain, target: self, action: #selector(recentTapped))
+        /// Try to put always the self reference, taking this as an habit, code more readable, and easy to know where is your var/function…
+        self.loadData()
+        self.title = "URL shortener"
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(self.shareTapped))
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Recent", style: .plain, target: self, action: #selector(self.recentTapped))
         
-        submitButton.layer.cornerRadius = 10
-        copyButton.layer.cornerRadius = 10
-        openPageButton.layer.cornerRadius = 10
+        self.submitButton.layer.cornerRadius = 10
+        self.copyButton.layer.cornerRadius = 10
+        self.openPageButton.layer.cornerRadius = 10
     }
     
     @IBAction func submitTapped(_ sender: Any) {
-        fetchData(longURL: urlEntry.text)
+        self.fetchData(longURL: self.urlEntry.text)
     }
     
     @IBAction func copyTapped(_ sender: Any) {
-        guard let link = currentResponse?.link else { return }
+        guard let link = self.currentResponse?.link else { return }
         UIPasteboard.general.string = link
-        showAlert(title: "Short URL is generated and copied to clipboard", message: link)
+        self.showAlert(title: "Short URL is generated and copied to clipboard", message: link)
     }
     
     @IBAction func openPageTapped(_ sender: Any) {
-        guard let link = currentResponse?.link else { return }
+        guard let link = self.currentResponse?.link else { return }
         guard let url = URL(string: link) else { return }
-        UIApplication.shared.open(url)
+        /// To improve the UX on your app, you can user the Safari View Controller, this allow to
+        /// show webcontent without leaving the app. (The configuration is not mandatory)
+        let config = SFSafariViewController.Configuration()
+        config.barCollapsingEnabled = true
+        config.entersReaderIfAvailable = false
+
+        let safariViewController = SFSafariViewController.init(url: url, configuration: config)
+        /// Is presented as modal beause the Safari View Controller already has his own "navigation bar"
+        self.present(safariViewController, animated: true, completion: nil)
     }
     
     func showAlert(title: String?, message: String?) {
         let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "OK", style: .default))
-        present(ac, animated: true)
+        self.present(ac, animated: true)
     }
     
     func fetchData(longURL: String?) {
         guard var longURL = longURL else { return }
-        if longURL == "" {
-            showAlert(title: "URL is empty!", message: "Please enter valid URL.")
+        /// On the first line you handle the nil value, and that it's ok, now to handle an empty value, check the
+        /// content of yout String counting the number of characters she has inside, because a String is a characters array.
+        /// To avoid if's without else cases and with and with and interrupted execurion try to use a guard, is more readable.
+        guard !longURL.isEmpty else {
+            self.showAlert(title: "URL is empty!", message: "Please enter valid URL.")
             return
         }
+        
         if !longURL.hasPrefix("https://") {
             longURL = "https://" + longURL
         }
@@ -67,25 +89,36 @@ class ViewController: UIViewController {
             longURL += "/"
         }
 
-        let request = fillRequest(longURL: longURL)
+        let request = self.fillRequest(longURL: longURL)
+        /// That [weak self] it's really cool because you can avoid strong references inside of your callback, this avoid the *memory leaks*
         let task = URLSession.shared.dataTask(with: request) { [weak self] data, response, error in
+            /// If you need to use self without handle the optionals, and avoid an strong reference, you can create a local self reference
+            /// this will be deallocated when your callback finish his task.
+            /// Other way to avoid optionals and strong references is changing [weak self] to [unowned self], but if your callback
+            /// try to access to your self, and this is already deallocated your app will crash. unowned is useful if you are sure 100% about
+            /// the allocate of your class (self).
+            guard let self = self else { return }
+            
             guard let data = data, error == nil else {
-                self?.showAlert(title: "Error", message: error?.localizedDescription)
+                self.showAlert(title: "Error", message: error?.localizedDescription)
                 return
             }
-            if self!.parseIsDataOK(data: data) {
-                if !self!.isLinkAlreadySaved(link: longURL) {
-                    self?.recentLinks.insert(self!.currentResponse!, at: 0)
-                    self?.saveData()
-                }
+            /// Again avoiding if's else, we can create a more readable code with a guard
+            guard self.parseIsDataOK(data: data) else {
                 DispatchQueue.main.async {
-                    self?.shortLinkView.text = self?.currentResponse?.link
-                    self?.reloadInputViews()
+                    self.showAlert(title: self.notOKResponse?.message, message: self.notOKResponse?.description)
                 }
-            } else {
-                DispatchQueue.main.async {
-                    self?.showAlert(title: self?.notOKResponse?.message, message: self?.notOKResponse?.description)
-                }
+                return
+            }
+            /// On this case the function don't need break the execution if the is no completed successfully
+            if !self.isLinkAlreadySaved(link: longURL) {
+                self.recentLinks.insert(self.currentResponse!, at: 0)
+                self.saveData()
+            }
+            
+            DispatchQueue.main.async { [weak self] in
+                self?.shortLinkView.text = self?.currentResponse?.link
+                self?.reloadInputViews()
             }
         }
         task.resume()
@@ -94,13 +127,15 @@ class ViewController: UIViewController {
     func fillRequest(longURL: String) -> URLRequest{
         let json: [String: Any] = ["long_url": longURL, "domain": "bit.ly"]
         let jsonData = try? JSONSerialization.data(withJSONObject: json)
-
-        var request = URLRequest(url: apiURL)
+        
+        /// Here is when you can check the user of prefix on your global constants, easily you can check if this is a local
+        /// var or not.
+        var request = URLRequest(url: k_apiURL)
         request.httpMethod = "POST"
         request.httpBody = jsonData
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         request.addValue("application/json", forHTTPHeaderField: "Accept")
-        request.setValue( "Bearer \(token)", forHTTPHeaderField: "Authorization")
+        request.setValue( "Bearer \(k_token)", forHTTPHeaderField: "Authorization")
         
         return request
     }
@@ -108,61 +143,64 @@ class ViewController: UIViewController {
     func parseIsDataOK(data: Data) -> Bool {
         let decoder = JSONDecoder()
         
-        if let jsonResponse = try? decoder.decode(ResponseDataOK.self, from: data) {
-            currentResponse = jsonResponse
-            return true
-        } else if let jsonResponse = try? decoder.decode(ResponseDataNotOK.self, from: data) {
-            notOKResponse = jsonResponse
+        /// Again more readable confitions with a guard instead an if else
+        guard let jsonResponse = try? decoder.decode(ResponseDataOK.self, from: data) else {
+            let jsonResponse = try? decoder.decode(ResponseDataNotOK.self, from: data)
+            self.notOKResponse = jsonResponse
+            return false
         }
         
-        return false
+        self.currentResponse = jsonResponse
+        return true
     }
     
     func isLinkAlreadySaved(link: String) -> Bool {
-        for item in recentLinks {
-            if item.long_url == link {
-                return true
-            }
-        }
-        
-        return false
+        /// Here you can minimize the code and increase his readability changing the loop
+        /// with an if nested, with the function contains. I guess the code is self explanatory.
+        return self.recentLinks.contains(where: { recentLink in
+            recentLink.long_url == link
+        })
     }
     
     func loadData() {
         let defaults = UserDefaults.standard
-        if let savedLinks = defaults.object(forKey: "links") as? Data {
-            let jsonDecoder = JSONDecoder()
-            do {
-                recentLinks = try jsonDecoder.decode([ResponseDataOK].self, from: savedLinks)
-            } catch {
-                print("Failed to load recent links.")
-            }
+        guard let savedLinks = defaults.object(forKey: "links") as? Data else {
+            return
+        }
+        
+        /// Here I decide to remove the initialization of a JSON decoder and initialize on the fly, because
+        /// less code with the same readability is better.
+        do {
+            self.recentLinks = try JSONDecoder().decode([ResponseDataOK].self, from: savedLinks)
+        } catch {
+            print("Failed to load recent links - \(error.localizedDescription)")
         }
     }
     
     func saveData() {
-        let jsonEncoder = JSONEncoder()
-        
-        if let savedLinks = try? jsonEncoder.encode(recentLinks) {
-            let defaults = UserDefaults.standard
-            defaults.set(savedLinks, forKey: "links")
-        } else {
+        guard let savedLinks = try? JSONEncoder().encode(self.recentLinks) else {
             print("Failed to save link.")
+            return
         }
+        
+        let defaults = UserDefaults.standard
+        defaults.set(savedLinks, forKey: "links")
     }
     
+    /// Nice update on @objc, I'm still finding the WWDC video, I contacted to Apple to locate it 😅
     @objc private func shareTapped() {
-        guard let link = currentResponse?.link else { return }
+        guard let link = self.currentResponse?.link else { return }
         let vc = UIActivityViewController(activityItems: ["Here is my short link:\n\(link)"], applicationActivities: [])
-        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
-        present(vc, animated: true)
+        vc.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
+        self.present(vc, animated: true)
     }
     
     @objc private func recentTapped() {
-        if let vc = storyboard?.instantiateViewController(identifier: "Detail") as? recentTableView {
-            vc.recentLinks = recentLinks
-            navigationController?.pushViewController(vc, animated: true)
+        guard let vc = storyboard?.instantiateViewController(identifier: "Detail") as? recentTableView else {
+            return
         }
+        vc.recentLinks = self.recentLinks
+        self.navigationController?.pushViewController(vc, animated: true)
     }
 }
 
